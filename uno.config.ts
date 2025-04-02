7a19d907e0d386ad618664789e638e892e8a0fc3
kevinhws <kevinshu1995@gmail.com>
2025-04-02 23:59:09 +0800

    feat: 新增 UnoCSS 預設並更新配置 新增 @unocss/preset-wind4 作為 UnoCSS 的預設之一，並 更新配置以包含文件系統內容。

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,11 @@
+import presetWind4 from '@unocss/preset-wind4';
 import { defineConfig, presetWebFonts } from 'unocss';
 
 export default defineConfig({
+    content: { filesystem: ['.vitepress/theme/**/*.{js,ts,vue}', '**/*.md'] },
     // ...UnoCSS options
     presets: [
+        presetWind4,
         presetWebFonts({
             provider: 'google',
             fonts: {
