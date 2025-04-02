c46038672fd3024f8425c5f121a6bbe741479892
kevinhws <kevinshu1995@gmail.com>
2025-04-02 23:59:18 +0800

    feat: 新增 Icon 元件以支援 SVG 圖示
    - 新增 Icon.vue 檔案，提供可重用的圖示元件
    - 支援自訂圖示大小及背景顏色
    - 使用 Iconify API 來載入 SVG 圖示

---
+++ .vitepress/theme/components/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+    <span
+        :class="['inline-block icon', props.size ?? 'size-4', props.bg ?? 'dark:bg-white bg-black']"
+        :style="`--svg: url('https://api.iconify.design/simple-icons/${props.icon}.svg')`"
+    />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    icon: string;
+    size?: string;
+    bg?: string;
+}>();
+</script>
+
+<style scoped>
+.icon {
+    -webkit-mask-image: var(--svg);
+    mask-image: var(--svg);
+    -webkit-mask-repeat: no-repeat;
+    mask-repeat: no-repeat;
+    -webkit-mask-size: 100% 100%;
+    mask-size: 100% 100%;
+}
+</style>
