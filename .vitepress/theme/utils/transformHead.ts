a6165d3be4b33abe6cdba14ede577ceec80948db
kevinhws <kevinshu1995@gmail.com>
2025-04-05 03:22:42 +0800

    feat(head): 更新 transformHead 函數以支援更多元資料
    - 新增對網站描述的支援
    - 增加對文章類型的判斷
    - 改善 meta 標籤的生成邏輯

--- .vitepress/theme/utils/transformHead.ts
+++ .vitepress/theme/utils/transformHead.ts
@@ -2,33 +2,46 @@ import { HeadConfig, TransformContext } from 'vitepress';
 import { formatISO } from 'date-fns';
 
 export default async function transformHead(context: TransformContext) {
-    const { pageData, siteConfig } = context;
+    const { pageData, siteConfig, siteData } = context;
     const head: HeadConfig[] = [];
     const themeConfig = siteConfig.userConfig.themeConfig;
-    const publishedTime = formatISO(new Date(pageData.frontmatter.date));
-    const lastUpdated = pageData.lastUpdated;
+
+    const title = pageData.frontmatter.title ?? '';
+    const description = pageData.frontmatter.description ?? siteData.description ?? '';
+    const publishedTime = formatISO(new Date(pageData.frontmatter.date ?? Date.now())) ?? '';
+    const lastUpdated = String(pageData.lastUpdated) ?? '';
     const relativePath = pageData.relativePath;
     const siteUrl = `${themeConfig.website}/${relativePath}`;
-    const authorName = themeConfig.author.name;
+    const authorName = themeConfig.author.name ?? '';
 
-    const category = pageData.frontmatter.category;
+    const category = pageData.frontmatter.category ?? '';
     const tags: string[] | undefined = pageData.frontmatter.tags;
 
-    head.push(['meta', { property: 'og:title', content: pageData.frontmatter.title }]);
-    head.push(['meta', { property: 'og:description', content: pageData.frontmatter.description }]);
-    head.push(['meta', { property: 'og:type', content: 'article' }]);
-    head.push(['meta', { property: 'article:author', content: authorName }]);
+    const isPost = !pageData.frontmatter.page;
+
+    // Open Graph
+    head.push(['meta', { property: 'og:title', content: title }]);
+    head.push(['meta', { property: 'og:description', content: description }]);
     head.push(['meta', { property: 'og:url', content: siteUrl }]);
     // head.push(['meta', { property: 'og:image', content: 'https://' }]);
 
-    head.push(['meta', { property: 'article:published_time', content: publishedTime }]);
-    head.push(['meta', { property: 'article:modified_time', content: String(lastUpdated) }]);
-    category && head.push(['meta', { property: 'article:section', content: category }]);
+    // 判斷是不是 post
+    if (isPost) {
+        head.push(['meta', { property: 'og:type', content: 'article' }]);
+        authorName && head.push(['meta', { property: 'article:author', content: authorName }]);
+        publishedTime &&
+            head.push(['meta', { property: 'article:published_time', content: publishedTime }]);
+        lastUpdated &&
+            head.push(['meta', { property: 'article:modified_time', content: lastUpdated }]);
+        category && head.push(['meta', { property: 'article:section', content: category }]);
 
-    if (tags && (tags ?? [])?.length > 0) {
-        tags.forEach((tag) => {
-            head.push(['meta', { property: 'article:tag', content: tag }]);
-        });
+        if (tags && (tags ?? [])?.length > 0) {
+            tags.forEach((tag) => {
+                head.push(['meta', { property: 'article:tag', content: tag }]);
+            });
+        }
+    } else {
+        head.push(['meta', { property: 'og:type', content: 'website' }]);
     }
 
     return head;
