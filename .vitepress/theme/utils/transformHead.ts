23325a0b865caf1a9e893e7f4f3988d4b5934699
kevinhws <kevinshu1995@gmail.com>
2025-04-05 04:03:22 +0800

    feat(head): 更新 transformHead 函數以支援結構化資料
    - 新增對於文章的結構化資料支援
    - 使用 format 函數格式化發佈和更新日期
    - 更新 Open Graph 標籤以包含語言屬性
    - 調整 robots.txt 檔案以允許所有爬蟲

--- .vitepress/theme/utils/transformHead.ts
+++ .vitepress/theme/utils/transformHead.ts
@@ -1,5 +1,5 @@
 import { HeadConfig, TransformContext } from 'vitepress';
-import { formatISO } from 'date-fns';
+import { format, formatISO } from 'date-fns';
 
 export default async function transformHead(context: TransformContext) {
     const { pageData, siteConfig, siteData } = context;
@@ -10,8 +10,13 @@ export default async function transformHead(context: TransformContext) {
 
     const title = pageData.frontmatter.title ?? '';
     const description = pageData.frontmatter.description ?? siteData.description ?? '';
-    const publishedTime = formatISO(new Date(pageData.frontmatter.date ?? Date.now()));
-    const lastUpdated = formatISO(new Date(pageData.lastUpdated ?? Date.now()));
+
+    const datePublishedTime = new Date(pageData.frontmatter.date ?? Date.now());
+    const dateLastUpdated = new Date(pageData.lastUpdated ?? Date.now());
+
+    const publishedTime = formatISO(datePublishedTime);
+    const lastUpdated = formatISO(dateLastUpdated);
+
     const relativePath = pageData.relativePath.replace('/index.md', '').replace('.md', '');
     const siteUrl = `${themeConfig.website}/${relativePath}`;
     const authorName = themeConfig.author.name ?? '';
@@ -22,7 +27,7 @@ export default async function transformHead(context: TransformContext) {
     head.push(['meta', { property: 'og:title', content: title }]);
     head.push(['meta', { property: 'og:description', content: description }]);
     head.push(['meta', { property: 'og:url', content: siteUrl }]);
-    // head.push(['meta', { property: 'og:image', content: 'https://' }]);
+    head.push(['meta', { property: 'og:locale', content: 'zh_TW' }]);
 
     if (isPost) {
         head.push(['meta', { property: 'og:type', content: 'article' }]);
@@ -42,6 +47,50 @@ export default async function transformHead(context: TransformContext) {
         head.push(['meta', { property: 'og:type', content: 'website' }]);
     }
     // Open Graph end =================
+
+    // Structured Data =================
+    if (isPost) {
+        head.push([
+            'script',
+            { type: 'application/ld+json' },
+            JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'Article',
+                mainEntityOfPage: {
+                    '@type': 'WebPage',
+                    '@id': siteUrl,
+                },
+                author: {
+                    '@type': 'Person',
+                    name: authorName,
+                },
+                headline: title,
+                // image: 'https://',
+                datePublished: format(datePublishedTime, 'yyyy-MM-dd'),
+                dateModified: format(dateLastUpdated, 'yyyy-MM-dd'),
+            }),
+        ]);
+    } else {
+        head.push([
+            'script',
+            { type: 'application/ld+json' },
+            JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'WebSite',
+                url: siteUrl,
+            }),
+        ]);
+    }
+    // TODO person
+    // {
+    //     "@context": "http://schema.org/",
+    //     "@type": "Person",
+    //     "name": "name",
+    //     "image": "photo url",
+    //     "url": "website url",
+    //     "jobTitle": "job title"
+    // }
+    // Structured Data end =================
 
     return head;
 }
